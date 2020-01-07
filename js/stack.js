fb3ae3e0946596a4deafdc8b7965a6e5f0daf4b6
ChenXin <729609320@qq.com>
2020-01-07 19:19:54 +0800

    Create stack.js

---
+++ js/stack.js
@@ -0,0 +1,30 @@
+//栈
+class Stack {
+    constructor() {
+        this.items = []
+    }
+
+    push(item) {
+        this.items.push(item)
+    }
+
+    pop() {
+        this.items.pop()
+    }
+
+    size() {
+        return this.items.length
+    }
+
+    isEmpty() {
+        return !this.items.length
+    }
+
+    clear() {
+        this.items = []
+    }
+
+    print() {
+        return this.items.toString()
+    }
+}
