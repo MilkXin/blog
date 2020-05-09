9c2227fd599c56d884dc9108a16b8ea8fc06d41a
ChenXin <729609320@qq.com>
2020-05-09 17:10:54 +0800

    Update 手写代码.js

--- js/手写代码.js
+++ js/手写代码.js
@@ -230,3 +230,13 @@ const curry = (fn, ...args) => {
 }
 const sum = curry(sumFn)
 console.log('>> ', sum(2)(3)(5)); */
+
+// 著名的Fisher–Yates shuffle 洗牌算法
+function shuffle(arr) {
+    let m = arr.length;
+    while (m > 1) {
+        let index = parseInt(Math.random() * m--);
+        [arr[index], arr[m]] = [arr[m], arr[index]];
+    }
+    return arr;
+}
