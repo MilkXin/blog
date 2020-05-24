c1b90992b8024cbf3904ecb39a53f318cc37bf82
chenxin <729609320@qq.com>
2020-05-24 23:34:09 +0800

    feat: Ajax

--- js/手写代码.js
+++ js/手写代码.js
@@ -23,28 +23,75 @@ function deepClone(arg) {
 }
 
 
+//call
+/* Function.prototype.myCall = function() {
+    const args = [...arguments]
+    var context = args.shift() || window
+    context.fn = this
+    const result = context.fn(...args)
+    delete context.fn
+    return result
+} */
+Function.prototype.myCall = function () {
+    let [thisArg, ...args] = [...arguments]
+    if (!thisArg) {
+        //context为null或是undefined
+        thisArg = typeof window === 'undefined' ? global : window
+    }
+    //this的指向是当前函数 func (func.call)
+    thisArg.func = this
+    //执行函数
+    let result = thisArg.func(...args)
+    //thisArg上并没有func属性，因此需要删除
+    delete thisArg.func
+    return result
+}
+
+//apply
+/* Function.prototype.myApply = function () {
+    const args = [...arguments]
+    var context = args.shift() || window
+    context.fn = this
+    const result = context.fn(...([].concat(...args)))
+    delete context.fn
+    return result
+} */
+Function.prototype.myApply = function (thisArg, rest) {
+    let result
+    if (!thisArg) {
+        thisArg = typeof window === 'undefined' ? global : window
+    }
+    thisArg.func = this
+    if (!rest) {
+        //第二个参数为null或undefined
+        result = thisArg.func()
+    } else {
+        result = thisArg.func(...rest)
+    }
+    delete thisArg.func
+    return result
+}
 
 //bind
-Function.prototype.bind2 = function() {
+Function.prototype.myBind = function () {
     const args = [...arguments]
     const t = args.shift()
     const self = this
-    return function() {
-        self.apply(t, args)
+    return function () {
+        return self.apply(t, args)
     }
 }
-
 /* function test() {
     console.log(this.name)
 }
-test.bind2({name: 'chen'})() */
+test.myBind({name: 'chen'})() */
 
 
 //防抖
-function debounce(fn, delay=500) {
+function debounce(fn, delay = 500) {
     let timer = null
-    
-    return function() {
+
+    return function () {
         if (timer) {
             clearTimeout(timer)
             return
@@ -61,7 +108,7 @@ function debounce(fn, delay=500) {
 function throttle(fn, delay) {
     let timer = null
 
-    return function() {
+    return function () {
         if (timer) {
             return
         }
@@ -107,7 +154,7 @@ console.log(fn(str)) */
 
 //数组扁平化
 function flat(arr) {
-    while(arr.some(item => Array.isArray(item))) {
+    while (arr.some(item => Array.isArray(item))) {
         arr = [].concat(...arr)
     }
     return arr
@@ -116,7 +163,7 @@ function flat(arr) {
 //数组去重
 function uniq(arr) {
     const result = []
-    for (let i=0; i<arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         if (!result.includes(arr[i])) {
             result.push(arr[i])
         }
@@ -133,67 +180,18 @@ console.log(arr2); //[1, 'a', 'b', 4, 5, 6] */
 function instance_of(left, right) {
     const prototype = right.prototype
     let proto = left.__proto__
-    while(true) {
-        if( proto === null) {
+    while (true) {
+        if (proto === null) {
             return false
         }
-        
-        if(prototype === proto) {
+
+        if (prototype === proto) {
             return true
         }
         proto = proto.__proto__
     }
 }
 
-
-//call
-/* Function.prototype.myCall = function() {
-    const args = [...arguments]
-    var context = args.shift() || window
-    context.fn = this
-    const result = context.fn(...args)
-    delete context.fn
-    return result
-} */
-Function.prototype.myCall = function() {
-    let [thisArg, ...args] = [...arguments]
-    if (!thisArg) {
-        //context为null或是undefined
-        thisArg = typeof window === 'undefined' ? global : window
-    }
-    //this的指向是当前函数 func (func.call)
-    thisArg.func = this
-    //执行函数
-    let result = thisArg.func(...args)
-    //thisArg上并没有func属性，因此需要删除
-    delete thisArg.func
-    return result
-}
-
-//apply
-/* Function.prototype.myApply = function () {
-    const args = [...arguments]
-    var context = args.shift() || window
-    context.fn = this
-    const result = context.fn(...([].concat(...args)))
-    delete context.fn
-    return result
-} */
-Function.prototype.myApply = function(thisArg, rest) {
-    let result
-    if (!thisArg) {
-        thisArg = typeof window === 'undefined' ? global : window
-    }
-    thisArg.func = this
-    if (!rest) {
-        //第二个参数为null或undefined
-        result = thisArg.func()
-    } else {
-        result = thisArg.func(...rest)
-    }
-    delete thisArg.func
-    return result
-}
 
 //手动实现new过程
 /*
@@ -203,7 +201,7 @@ Function.prototype.myApply = function(thisArg, rest) {
 4. 如果构造函数中没有返回其它对象，那么返回this，即创建的这个的新对象，否则，返回构造函数中返回的对象。 */
 function _new() {
     let target = {}
-    let [ constructor, ...args ] = [...arguments]
+    let [constructor, ...args] = [...arguments]
     target.__proto__ = constructor.prototype
     let result = constructor.apply(target, args)
     if (result && (typeof result === 'object' || typeof result === 'function')) {
@@ -213,6 +211,21 @@ function _new() {
     return target
 }
 
+//Ajax
+let xhr
+if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest()
+} else {
+    xhr = new ActiveXObject('Microsoft.XMLHTTP')
+}
+xhr.open('get', '/api', true) //默认为true(异步)
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const result = xhr.responseText
+        console.log(result)
+    }
+}
+xhr.send(null)
 
 //函数柯里化
 //函数柯里化是把接受多个参数的函数变换成接受一个单一参数（最初函数的第一个参数）的函数，并且返回接受余下的参数而且返回结果的新函数的技术。
@@ -221,8 +234,8 @@ function _new() {
 //     3. 延迟执行 – 返回新函数，等待执行
 const curry = (fn, ...args) => {
     return args.length < fn.length
-    ? (...arguments) => curry(fn, ...args, ...arguments)
-    : fn(...args)
+        ? (...arguments) => curry(fn, ...args, ...arguments)
+        : fn(...args)
 }
 
 /* function sumFn(a, b, c) {
@@ -244,12 +257,12 @@ function shuffle(arr) {
 
 //汉诺塔
 function hanio(n, x, y, z) {
-    if(n === 0) {
+    if (n === 0) {
         return
     }
-    hanio(n-1, x, z, y)
+    hanio(n - 1, x, z, y)
     move(n, x, z)
-    hanio(n-1, y, x, z)
+    hanio(n - 1, y, x, z)
 }
 function move(id, from, to) {
     console.log(`把${id}从${from} --> ${to}`)
@@ -258,7 +271,7 @@ function move(id, from, to) {
 
 //斐波拉契数列
 function fibonacci(n) {
-    if(n<=1){
+    if (n <= 1) {
         return 1
     }
     var n0 = 1, n1 = 1, sum;
